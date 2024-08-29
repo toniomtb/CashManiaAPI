e3bf352e07441682ae3e28ef75e36437caeadfa5
Antonio Marra <antonio.marra@energee3.com>
2024-08-29 11:24:46 +0200

    added CategoryServiceTests class

---
+++ CashManiaAPI/CashManiaAPI.Tests/CategoryServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using CashManiaAPI.Data;
+using CashManiaAPI.Services;
+using Moq;
+
+namespace CashManiaAPI.Tests;
+
+class CategoryServiceTests
+{
+    private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+    private readonly CategoryService _categoryService;
+
+    public CategoryServiceTests()
+    {
+        _mockUnitOfWork = new Mock<IUnitOfWork>();
+        _categoryService = new CategoryService(_mockUnitOfWork.Object);
+    }
+}
